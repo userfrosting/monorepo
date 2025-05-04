403647ba90fb34c8ccc206637fb16cd2575ea715
Louis Charette <charette.louis@gmail.com>
2025-05-03 23:09:47 -0400

    Cleanup other package unused tools

--- packages/skeleton/.php-cs-fixer.headers.php
+++ packages/skeleton/.php-cs-fixer.headers.php
@@ -1,20 +1,21 @@
 <?php
 
-$header = 'UserFrosting Admin Sprinkle (http://www.userfrosting.com)
+$header = 'UserFrosting (http://www.userfrosting.com)
 
-@link      https://github.com/userfrosting/sprinkle-admin
+@link      https://github.com/userfrosting/UserFrosting
 @copyright Copyright (c) 2013-2024 Alexander Weissman & Louis Charette
-@license   https://github.com/userfrosting/sprinkle-admin/blob/master/LICENSE.md (MIT License)';
+@license   https://github.com/userfrosting/UserFrosting/blob/master/LICENSE.md (MIT License)';
 
 $rules = [
     'header_comment' => [
-        'header'       => $header
+        'header'       => $header,
     ]
 ];
 $finder = PhpCsFixer\Finder::create()
     ->in([
         __DIR__ . '/app/src',
         __DIR__ . '/app/tests',
+        __DIR__ . '/public'
     ]);
 $config = new PhpCsFixer\Config();
 
