d92cbc36de65c7d9c561555b629c0f101c2ac770
Louis Charette <charette.louis@gmail.com>
2025-08-29 20:21:33 -0400

    Update vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,12 +6,16 @@ export default defineConfig({
         coverage: {
             enabled: true,
             reportsDirectory: './_meta/_coverage',
-            include: [
-                'packages/**/app/assets/**/*.*',
-                'packages/**/src/**/*.*'
-            ],
+            include: ['packages/**/app/assets/**/*.*', 'packages/**/src/**/*.*']
         },
         reporters: ['default', 'junit'],
-        outputFile: './_meta/junit_frontend.xml'
+        outputFile: './_meta/junit_frontend.xml',
+        projects: [
+            './packages/theme-pink-cupcake/vite.config.ts',
+            './packages/sprinkle-core/vite.config.ts',
+            './packages/sprinkle-account/vite.config.ts',
+            './packages/sprinkle-admin/vite.config.ts',
+            './packages/skeleton/vite.config.ts'
+        ]
     }
 })
--- vitest.workspace.js
+++
@@ -1,9 +0,0 @@
-import { defineWorkspace } from "vitest/config";
-
-export default defineWorkspace([
-    "./packages/theme-pink-cupcake/vite.config.ts",
-    "./packages/sprinkle-core/vite.config.ts",
-    "./packages/sprinkle-account/vite.config.ts",
-    "./packages/sprinkle-admin/vite.config.ts",
-    "./packages/skeleton/vite.config.ts",
-]);
